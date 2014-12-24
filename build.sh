0b9defce407f30bc528fcca08309567076cf0a48
zaclimon <isaacpateau05@gmail.com>
2014-12-24 16:52:48 -0500

    Android Lollipop for Flo, r20, Launching!!

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ JOBS=`grep -c "processor" /proc/cpuinfo`
 
 if [[ "$1" =~ "cm" || "$1" =~ "CM" ]] ; then
 git checkout sm-5.0-cm
-zipfile="franco.Kernel-SaberMod-r17.2-CM.zip"
+zipfile="franco.Kernel-SaberMod-r20-CM.zip"
 else
 git checkout sm-5.0
-zipfile="franco.Kernel-SaberMod-r17.2.zip"
+zipfile="franco.Kernel-SaberMod-r20.zip"
 fi
 
 
